739f11aa3ec14f301a14a35ce3c9482068bee0ff
Your Name <you@example.com>
2020-09-24 15:54:04 +0100

    Update

---
+++ Python3/setup.py
@@ -0,0 +1,18 @@
+import sys
+from cx_Freeze import setup, Executable
+import config
+
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {"packages": ["os"]}
+
+# GUI applications require a different base on Windows (the default is for a
+# console application).
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(  name = "GUI Hangman",
+        version = config.BUILD_VERSION,
+        description = "A basic hangman game.",
+        options = {"build_exe": build_exe_options},
+        executables = [Executable("main.py", base=base)])
